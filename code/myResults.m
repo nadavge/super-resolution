9f857631df456cb8d6b589ee35e776d79ae3f224
Nadav Geva <nadavgeva4@gmail.com>
2016-01-29 00:17:13 +0200

    Add tic to myResults

--- code/myResults.m
+++ code/myResults.m
@@ -4,7 +4,9 @@ for filename=images,
     filename = filename{1};
     im = im2double(imread(filename));
     bigim = imresize(im, 2, 'cubic');
+    tic;
     result = colorSuperResolution(im);
+    toc;
     
     figure; imshow(im);
     figure; imshow(bigim);
